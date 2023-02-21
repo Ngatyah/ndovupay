75e7c166832f6aae5ee46d75aca1e95fb80d2e0a
ngatyah <davidngatyah@gmail.com>
2023-02-21 15:55:58 +0300

    Add NavBar

--- src/Pages/NavBar/index.jsx
+++ src/Pages/NavBar/index.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { FiHelpCircle } from "react-icons/fi";
+import { MdNotificationsNone, MdOutlineAccountCircle } from "react-icons/md";
 
 const NavBar = () => {
-  return <div className="h-10">NavBar</div>;
+  return (
+    <div className="flex justify-between items-center h-14 mx-4">
+      <h1 className="tezt-2xl font-bold">
+        Ndovu<span className="text-[#DB2A12]">Pay</span>
+      </h1>
+      <div className="flex gap-8">
+        <div className="flex gap-2 text-[#8FE1A1]">
+          <FiHelpCircle size={28} />
+          <h2 className="text-[#000]"> Help</h2>
+        </div>
+        <div className="">
+          <MdNotificationsNone size={28} />
+        </div>
+        <div className="flex gap-2">
+          <MdOutlineAccountCircle size={28} />
+          Austine
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
